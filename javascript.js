a5f4a8fb327849729c20497b88e58c2bb3c58ef4
Fredrik Uddenfeldt <fredrikuddenfeldt@fredriks-air.lan>
2023-05-08 22:45:18 +0200

    partial solutions

--- javascript.js
+++ javascript.js
@@ -1,5 +1,44 @@
-let a = 10
-let b = 20
-const max = 57
-let actual = max - 13
-let percentage = actual/max
+function add7(x) {
+    return x + 7;
+}
+
+function multiply(a, b) {
+    return a * b;
+} 
+
+let x = 2;
+
+
+function capitalize(text) {
+    return 
+        text.toUpperCase(text.slice(0,1))
+        text.toLowerCase(text.slice(2,100))
+
+
+}
+
+
+console.log(capitalize("hELLo")) 
+
+/* const string = 'hELLO'
+
+console.log(string.slice(0,1)) + (string.slice(2,100)
+
+
+);
+
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+console.log(str.slice(0,100000));
+
+
+/* 
+console.log(str.slice(4, 19));
+// Expected output: "quick brown fox"
+
+console.log(str.slice(-4));
+// Expected output: "dog."
+
+console.log(str.slice(-9, -5));
+// Expected output: "lazy" */
